4e4e3a0116a59e8f500079f514152c3ddb05bbe7
anton <anton.sh.local@gmail.com>
2026-09-03 08:06:29 +0400

    fix shaders

--- hyprshade/shaders/vibrance.glsl
+++ hyprshade/shaders/vibrance.glsl
@@ -8,18 +8,18 @@ out vec4 fragColor;
 const vec3 rgbBalance = vec3(1.0, 1.0, 1.0);
 const float vibranceStrength = 0.10;
 const float saturationThreshold = 0.15;
-const vec3 vibranceCoeffient = rgbBalance * -vibranceStrength;
+const vec3 vibranceCoefficient = rgbBalance * -vibranceStrength;
 
 void main()
 {
     vec4 pixelColor = texture(tex, v_texcoord);
     vec3 color = pixelColor.rgb;
-    vec3 lumaCoefficent = vec3(0.212656, 0.715158, 0.072186);
-    float luma = dot(lumaCoefficent, color);
+    vec3 lumaCoefficient = vec3(0.212656, 0.715158, 0.072186);
+    float luma = dot(lumaCoefficient, color);
     float maxColor = max(color.r, max(color.g, color.b));
     float minColor = min(color.r, min(color.g, color.b));
     float saturation = maxColor - minColor;
-    vec3 vibranceTransform = (sign(vibranceCoeffient) * saturation - 1.0) * vibranceCoeffient + 1.0;
+    vec3 vibranceTransform = (sign(vibranceCoefficient) * saturation - 1.0) * vibranceCoefficient + 1.0;
     vec3 saturatedColor = mix(vec3(luma), color, vibranceTransform);
     float fadeFactor = smoothstep(saturationThreshold, saturationThreshold + 0.3, saturation) * smoothstep(0.05, 0.25, luma);
     vec3 adjustedColor = mix(color, saturatedColor, fadeFactor);
